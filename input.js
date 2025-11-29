19c8661f102f7d5e0e204a66efa2c73263019625
Musta-Fast <mshrhussain243@gmail.com>
2025-11-29 14:53:13 +0500

    Remove input.js and input.py from repo and ignore them

--- input.js
+++
@@ -1,3 +0,0 @@
-// ❌ Vulnerable Pattern – directly inserting user input into the DOM
-const name = window.location.search.replace("?name=", "");
-document.getElementById("output").innerHTML = name; 
